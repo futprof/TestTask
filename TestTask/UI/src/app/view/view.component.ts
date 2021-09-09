af1dac25c4d66a1a8f3fc3da3f4d446a9b8c6c28
Your Name <Vishnyakov@agrooven.com>
2021-09-09 17:59:18 +0300

    Added sort actions on UI

--- TestTask/UI/src/app/view/view.component.ts
+++ TestTask/UI/src/app/view/view.component.ts
@@ -18,21 +18,39 @@ export class ViewComponent implements OnInit {
   }
 
   taskList:any=[];
+  taskListPoor:any=[];
 
   refreshTasksList(){
     this.service.getAllTasks().subscribe(data=>{
       this.taskList = data;
+      this.taskListPoor = data;
     });
   }
 
 
-  fieldsChange(values:any):void {
+  statusChange(values:any):void {
     var formData: any = new FormData();
     formData.append("id", values.currentTarget.id);
     formData.append("status", values.currentTarget.checked);
     
     this.service.changeStatus(formData).subscribe(x=>{console.log(x);});    
 
+  }
+
+  sortResult(property:any, flag:boolean){
+    this.taskList = this.taskListPoor.sort(function(a:any, b:any){
+      if(flag){
+        return (a[property]>b[property]) ? 1: ((a[property]<b[property]) ?-1 : 0) ;
+      } else return (b[property]>a[property]) ? 1: ((b[property]<a[property]) ?-1: 0) ;
+    });
+  }
+
+  sortByDate(property:any, flag:boolean){
+    this.taskList = this.taskListPoor.sort(function(a:any, b:any){
+      if(flag){
+        return <any>new Date(b.DueDate) - <any>new Date(a.DueDate);
+      } else return <any>new Date(a.DueDate) - <any>new Date(b.DueDate);
+    });
   }
 
 }
