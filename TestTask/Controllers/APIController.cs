6914c0d4a2468e9fe0b97ed306c6d7d7d5727a96
Your Name <Vishnyakov@agrooven.com>
2021-09-14 10:43:41 +0300

    Added try catch for API controller

--- TestTask/Controllers/APIController.cs
+++ TestTask/Controllers/APIController.cs
@@ -5,6 +5,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using TestTask.Models;
 using TestTask.Models.DB;
 
 namespace TestTask.Controllers
@@ -12,6 +13,7 @@ namespace TestTask.Controllers
     public class APIController : ControllerBase
     {
         private readonly Storage _storage;
+        private ErrorMessage _errorMessage;
         public APIController(DbContextOptions<Models.DB.AppContext> options)
         {
             _storage = new Storage(options);
@@ -21,79 +23,186 @@ namespace TestTask.Controllers
         [Route("api/get-all-tasks")]
         public string GetAll(int page, int results)
         {
-            var tasks = _storage.GetTasks(page, results);
-            var json = JsonConvert.SerializeObject(tasks, Formatting.Indented);
-            return json;
+            try
+            {
+                var tasks = _storage.GetTasks(page, results);
+                var json = JsonConvert.SerializeObject(tasks, Formatting.Indented);
+                return json;
+            }
+            catch (Exception)
+            {
+                _errorMessage = new ErrorMessage {
+                    DateOffset = DateTimeOffset.UtcNow,
+                    Message = "Bad reqest.",
+                    Method = ControllerContext.ActionDescriptor.ActionName }; 
+                return JsonConvert.SerializeObject(_errorMessage, Formatting.Indented);
+                throw;
+            }
+           
         }
 
         [HttpDelete]
         [Route("api/del-task/{id}")]
         public bool RemoveTask(string id)
         {
-            return _storage.RemoveTask(id);
+            try
+            {
+                return _storage.RemoveTask(id);
+            }
+            catch (Exception)
+            {
+                return false;
+            }
+            
         }
 
         [HttpPost]
         [Route("api/change-status")]
         public bool ChangeTaskStatus(string id, bool status)
         {
-            return _storage.ChengeTaskStatus(id, status);
+            try
+            {
+                return _storage.ChengeTaskStatus(id, status);
+            }
+            catch (Exception)
+            {
+                return false;
+            }
         }
 
         [HttpGet]
         [Route("api/get-task/{id}")]
         public string GetTask(string id)
         {
-            var task = _storage.GetTask(id);
-            var json = JsonConvert.SerializeObject(task, Formatting.Indented);
-            return json;
+            try
+            {
+                var task = _storage.GetTask(id);
+                var json = JsonConvert.SerializeObject(task, Formatting.Indented);
+                return json;
+            }
+            catch (Exception)
+            {
+                _errorMessage = new ErrorMessage
+                {
+                    DateOffset = DateTimeOffset.UtcNow,
+                    Message = "Bad reqest.",
+                    Method = ControllerContext.ActionDescriptor.ActionName
+                };
+                return JsonConvert.SerializeObject(_errorMessage, Formatting.Indented);                
+            }           
         }
 
         [HttpPost]
         [Route("api/update-task")]
         public string UpdateTask(Models.Task task)
         {
-            return _storage.UpdateTask(task);
+            try
+            {
+                return _storage.UpdateTask(task);
+            }
+            catch (Exception)
+            {
+                _errorMessage = new ErrorMessage
+                {
+                    DateOffset = DateTimeOffset.UtcNow,
+                    Message = "Bad reqest.",
+                    Method = ControllerContext.ActionDescriptor.ActionName
+                };
+                return JsonConvert.SerializeObject(_errorMessage, Formatting.Indented);
+            }
+           
         }
 
         [HttpPost]
         [Route("api/add-task")]
         public JsonResult AddTask(Models.Task task)
-        {
-            if (!ModelState.IsValid)
+        {            
+            try
             {
-                return new JsonResult("Task is not valid!");
+                if (!ModelState.IsValid)
+                {
+                    return new JsonResult("Task is not valid!");
+                }
+                _storage.AddTask(task);
+                return new JsonResult("Sucess!");
             }
-            _storage.AddTask(task);
-            return new JsonResult("Sucess!");
+            catch (Exception)
+            {
+                _errorMessage = new ErrorMessage
+                {
+                    DateOffset = DateTimeOffset.UtcNow,
+                    Message = "Bad reqest.",
+                    Method = ControllerContext.ActionDescriptor.ActionName
+                };
+                return new JsonResult(JsonConvert.SerializeObject(_errorMessage, Formatting.Indented));                
+            }
         }
 
         [HttpGet]
         [Route("api/tasks-amount")]
         public string GetTasksAmount()
-        {
-            var json = JsonConvert.SerializeObject(_storage.GetTasksAmount());
-            return json;
+        {           
+            try
+            {
+                var json = JsonConvert.SerializeObject(_storage.GetTasksAmount());
+                return json;
+            }
+            catch (Exception)
+            {
+                _errorMessage = new ErrorMessage
+                {
+                    DateOffset = DateTimeOffset.UtcNow,
+                    Message = "Bad reqest.",
+                    Method = ControllerContext.ActionDescriptor.ActionName
+                };
+                return JsonConvert.SerializeObject(_errorMessage, Formatting.Indented);
+                throw;
+            }
         }
 
         [HttpGet]
         [Route("api/search-by-name/{name}")]
         public string SearchTask(string name)
         {
-            var tasks = _storage.SearchTask(name);
-            var json = JsonConvert.SerializeObject(tasks, Formatting.Indented);
-            return json;
+            try
+            {
+                var tasks = _storage.SearchTask(name);
+                var json = JsonConvert.SerializeObject(tasks, Formatting.Indented);
+                return json;
+            }
+            catch (Exception)
+            {
+                _errorMessage = new ErrorMessage
+                {
+                    DateOffset = DateTimeOffset.UtcNow,
+                    Message = "Bad reqest.",
+                    Method = ControllerContext.ActionDescriptor.ActionName
+                };
+                return JsonConvert.SerializeObject(_errorMessage, Formatting.Indented);
+            }
+           
         }
 
         [HttpGet]
         [Route("api/search-by-status/{status}")]
         public string SearchTask(bool status)
         {
-            var tasks = _storage.SearchTask(status);
-            var json = JsonConvert.SerializeObject(tasks, Formatting.Indented);
-            return json;
+            try
+            {
+                var tasks = _storage.SearchTask(status);
+                var json = JsonConvert.SerializeObject(tasks, Formatting.Indented);
+                return json;
+            }
+            catch (Exception)
+            {
+                _errorMessage = new ErrorMessage
+                {
+                    DateOffset = DateTimeOffset.UtcNow,
+                    Message = "Bad reqest.",
+                    Method = ControllerContext.ActionDescriptor.ActionName
+                };
+                return JsonConvert.SerializeObject(_errorMessage, Formatting.Indented);
+            }           
         }
-
-
     }
 }
---
+++ TestTask/Models/ErrorMessage.cs
@@ -0,0 +1,16 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TestTask.Models
+{
+    [JsonObject]
+    public class ErrorMessage
+    {
+        public DateTimeOffset DateOffset { get; set; }
+        public string Method { get; set; }
+        public string Message { get; set; }
+    }
+}
